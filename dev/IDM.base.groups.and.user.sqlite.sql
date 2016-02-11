ac8dd8e1d44d7c057e4d3056c54d9b3d0bc97a6f
gessinger-hj <gessinger.hj@gmail.com>
2016-02-11 10:57:35 +0100

    DB Model 2

--- dev/IDM.base.groups.and.user.sqlite.sql
+++ dev/IDM.base.groups.and.user.sqlite.sql
@@ -15,6 +15,38 @@ INSERT INTO T_IDENTITY_TO_IDENTITY ( PARENT_IDENTITY_KEY, IDENTITY_KEY )
 VALUES ( ( select IDENTITY_KEY from T_IDENTITY where IDENTITY_NAME='RootGroup' ), ( select IDENTITY_KEY from T_IDENTITY where IDENTITY_NAME='root' ) ) ;
 
 
+-- Admin ----------------------------------------------------------
+INSERT INTO T_IDENTITY ( IDENTITY_NAME, IDENTITY_TYPE_KEY, DESCRIPTION, ENABLED, LOGIN_ENABLED )
+VALUES ( 'AdminGroup', ( select IDENTITY_TYPE_KEY from T_IDENTITY_TYPE where IDENTITY_TYPE_NAME='Group' ), 'Root group', '1', '0' ) ;
+
+INSERT INTO T_IDENTITY ( IDENTITY_NAME, IDENTITY_TYPE_KEY, DESCRIPTION, ENABLED, LOGIN_ENABLED, PWD )
+VALUES ( 'admin', ( select IDENTITY_TYPE_KEY from T_IDENTITY_TYPE where IDENTITY_TYPE_NAME='Person' ), 'admin user', '1', '1', '654321' ) ;
+
+INSERT INTO T_IDENTITY_TO_IDENTITY ( PARENT_IDENTITY_KEY, IDENTITY_KEY )
+VALUES ( ( select IDENTITY_KEY from T_IDENTITY where IDENTITY_NAME='AdminGroup' ), ( select IDENTITY_KEY from T_IDENTITY where IDENTITY_NAME='admin' ) ) ;
+
+-- Customer Group ----------------------------------------------------
+INSERT INTO T_IDENTITY ( IDENTITY_NAME, IDENTITY_TYPE_KEY, DESCRIPTION, ENABLED, LOGIN_ENABLED )
+VALUES ( 'CustomerGroup', ( select IDENTITY_TYPE_KEY from T_IDENTITY_TYPE where IDENTITY_TYPE_NAME='Group' ), 'Customer group', '1', '0' ) ;
+
+-- Customer ----------------------------------------------------------
+
+INSERT INTO T_RIGHT ( RIGHT_NAME ) VALUES ( 'CAN_PURCHASE_PRODUCTS' ) ; 
+
+INSERT INTO T_IDENTITY_TO_RIGHT ( IDENTITY_KEY, RIGHT_KEY )
+VALUES ( (select IDENTITY_KEY from T_IDENTITY where IDENTITY_NAME='CustomerGroup')
+       , (select RIGHT_KEY from T_RIGHT where RIGHT_NAME='CAN_PURCHASE_PRODUCTS')
+       );
+
+INSERT INTO T_IDENTITY ( IDENTITY_NAME, IDENTITY_TYPE_KEY, DESCRIPTION, ENABLED, LOGIN_ENABLED, PWD )
+VALUES ( 'miller', ( select IDENTITY_TYPE_KEY from T_IDENTITY_TYPE where IDENTITY_TYPE_NAME='Person' ), 'customer and admin', '1', '1', '654321' ) ;
+
+INSERT INTO T_IDENTITY_TO_IDENTITY ( PARENT_IDENTITY_KEY, IDENTITY_KEY )
+VALUES ( ( select IDENTITY_KEY from T_IDENTITY where IDENTITY_NAME='AdminGroup' ), ( select IDENTITY_KEY from T_IDENTITY where IDENTITY_NAME='miller' ) ) ;
+
+INSERT INTO T_IDENTITY_TO_IDENTITY ( PARENT_IDENTITY_KEY, IDENTITY_KEY )
+VALUES ( ( select IDENTITY_KEY from T_IDENTITY where IDENTITY_NAME='CustomerGroup' ), ( select IDENTITY_KEY from T_IDENTITY where IDENTITY_NAME='miller' ) ) ;
+
 -- Rights ---------------------------------------------------------
 
 INSERT INTO T_RIGHT ( RIGHT_NAME ) VALUES ( 'CAN_EDIT_USER_OF' ) ; 
@@ -25,29 +57,20 @@ INSERT INTO T_RIGHT ( RIGHT_NAME ) VALUES ( 'CAN_EDIT_GROUP' ) ;
 
 -- Group To Rights -------------------------------------------------
 
-INSERT INTO T_IDENTITY_TO_RIGHT ( IDENTITY_KEY, RIGHT_KEY, RIGHT_CONTEXT, RIGHT_VALUE )
+INSERT INTO T_IDENTITY_TO_RIGHT ( IDENTITY_KEY, RIGHT_KEY, RIGHT_VALUE )
 VALUES ( (select IDENTITY_KEY from T_IDENTITY where IDENTITY_NAME='RootGroup')
        , (select RIGHT_KEY from T_RIGHT where RIGHT_NAME='CAN_EDIT_USER_OF')
        , '*'
-       , '*'
        );
 
-INSERT INTO T_IDENTITY_TO_RIGHT ( IDENTITY_KEY, RIGHT_KEY, RIGHT_CONTEXT, RIGHT_VALUE )
+INSERT INTO T_IDENTITY_TO_RIGHT ( IDENTITY_KEY, RIGHT_KEY )
 VALUES ( (select IDENTITY_KEY from T_IDENTITY where IDENTITY_NAME='RootGroup')
        , (select RIGHT_KEY from T_RIGHT where RIGHT_NAME='CAN_EDIT_USER')
-       , '*'
-       , 'true'
        );
 
-INSERT INTO T_IDENTITY_TO_RIGHT ( IDENTITY_KEY, RIGHT_KEY, RIGHT_CONTEXT, RIGHT_VALUE )
+INSERT INTO T_IDENTITY_TO_RIGHT ( IDENTITY_KEY, RIGHT_KEY )
 VALUES ( (select IDENTITY_KEY from T_IDENTITY where IDENTITY_NAME='RootGroup')
        , (select RIGHT_KEY from T_RIGHT where RIGHT_NAME='CAN_EDIT_GROUP')
-       , '*'
-       , 'true'
        );
 
 ----------------------------------------------------------------------
-	   
-commit ;
-
-exit
--- dev/sqlite-sql.js-test.js
+++ dev/sqlite-sql.js-test.js
@@ -1,24 +1,21 @@
+#!/usr/bin/env node
+
 /* 
 * @Author: Hans Jürgen Gessinger
 * @Date:   2016-02-09 19:42:51
-* @Last Modified by:   HG02055
-* @Last Modified time: 2016-02-09 20:12:10
-* File-name: sqlite.test
+* @Last Modified by:   hg02055
+* @Last Modified time: 2016-02-10 17:53:11
+* File-name: sqlite-sql.js-test
 */
 
 'use strict';
-
-var sqlite3 = require('sql.js') ;
-var db = new sqlite3.Database('sidds.db');
-
+var T = require ( "gepard" ) ;
+var L = require ( "gepard" ).LogFile ;
 var fs = require('fs');
 var SQL = require('sql.js');
 var filebuffer = fs.readFileSync ( 'sidds.db' ) ;
-
-// Load the db
 var db = new SQL.Database(filebuffer);
-
 var res = db.exec ( "SELECT * FROM T_IDENTITY" ) ;
 
-console.log ( res[0] ) ;
-db.close();
+// console.log ( res[0] ) ;
+// db.close();
