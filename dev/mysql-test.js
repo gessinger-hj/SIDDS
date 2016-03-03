f09a2c191dd19de457fd6218c7855ecb3e6c4c78
gessinger-hj <gessinger.hj@gmail.com>
2016-03-03 20:01:32 +0100

    wait.for

--- dev/mysql-test.js
+++ dev/mysql-test.js
@@ -6,18 +6,18 @@ var T = require ( "gepard" ) ;
   var connection =  mysql.createConnection ( dburl ) ;
   connection.connect();
   connection.query( "use sidds" );
-var sql = "select IDENTITY_NAME from t_identity where IDENTITY_NAME=?";	
+var sql = "select IDENTITY_NAME, IDENTITY_KEY from t_identity where IDENTITY_NAME=?";	
 
 function loadDataFromDatabase()
 {
-connection.query( sql, function(err, rows, fields ){
+connection.query( sql, [ 'miller' ], function(err, rows, fields ){
     // console.log ( fields ) ;
   	if(err)	{
   		throw err;
   	}
     else
     {
-// console.log ( rows ) ;
+console.log ( rows[0] ) ;
   		for ( var i = 0 ; i < rows.length ; i++ )
   		{
       // console.log( rows[i].IDENTITY_NAME );
@@ -27,22 +27,33 @@ connection.query( sql, function(err, rows, fields ){
   	connection.end() ;
   });
 }
+loadDataFromDatabase() ;
+return ;
 var wait = require ( "wait.for" ) ;
-function useLoadedData ( d )
-{
-  console.log ( d ) ;
-}
 
-var data ;
+console.log ( connection ) ;
+connection.q = function(_sql, params, stdCallback){ 
+             this.query(_sql,params, function(err,rows,columns){ 
+                                 return stdCallback(err,{rows:rows,columns:columns}); 
+                         });
+ }
 function process()
 {
-  if ( !data )
-  {
-    data = wait.forMethod ( connection, "query", sql, [ 'miller' ] ) ;
-    console.log ( data ) ;
-    data = wait.forMethod ( connection, "query", sql, [ 'admin' ] ) ;
-    console.log ( "data[0].IDENTITY_NAME=" + data[0].IDENTITY_NAME ) ;
+
+  try {
+    var result = wait.forMethod(connection, "q", sql, [ 'miller' ] ); 
+    console.log(result.rows);
+    console.log(result.columns);
+  } 
+  catch(err) {
+     console.log(err);
   }
   connection.end() ;
+  // var data = wait.forMethod ( connection, "query", sql, [ 'miller' ] ) ;
+  // console.log ( data ) ;
+  // data = wait.forMethod ( connection, "query", sql, [ 'admin' ] ) ;
+  // console.log ( "data[0].IDENTITY_NAME=" + data[0].IDENTITY_NAME ) ;
+  // console.log ( "data[0].IDENTITY_KEY=" + data[0].IDENTITY_KEY ) ;
+  // connection.end() ;
 }
 wait.launchFiber(process);
