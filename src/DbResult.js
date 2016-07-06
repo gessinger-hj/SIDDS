d0bb35772816679a04327eca00b5b2cd4dfe423a
Hans Jürgen Gessinger <gessinger.hj@gmail.com>
2016-07-06 22:55:57 +0200

    DbResult

--- src/DbResult.js
+++ src/DbResult.js
@@ -2,18 +2,15 @@
 * @Author: Hans Jürgen Gessinger
 * @Date:   2016-04-26 16:38:22
 * @Last Modified by:   Hans Jürgen Gessinger
-* @Last Modified time: 2016-06-28 19:40:00
+* @Last Modified time: 2016-07-06 15:15:10
 */
 
 'use strict';
 let DbResult = function ( error, tag, res )
 {
 	this.error  = error ;
-	this.list = [] ;
-	if ( res )
-	{
-		this.add ( tag, res ) ;
-	}
+	this.resultList = [] ;
+	this.add ( tag, res ) ;
 };
 DbResult.prototype.add = function ( tag, res )
 {
@@ -22,8 +19,11 @@ DbResult.prototype.add = function ( tag, res )
 	{
 		throw new Error ( "Missing tag" ) ;
 	}
-	let item = {} ;
-	item[tag] = res ;
-	this.list.push ( item ) ;
-}
+	let item = { name:tag, rows: res } ;
+	this.resultList.push ( item ) ;
+};
+DbResult.prototype.getResultList = function()
+{
+	return this.resultList ;
+};
 module.exports = DbResult ;
